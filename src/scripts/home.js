4da9f766379a0cf30d129a3aef815ecaac07f5ee
titi20117 <fernandmoudio1@gmail.com>
2020-02-26 22:43:54 +0300

    modal burger

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,4 +1,4 @@
-let  navSolutionTitle = document.querySelector(".block-page__item_header__intro__nav__solution_title");
+let navSolutionTitle = document.querySelector(".block-page__item_header__intro__nav__solution_title");
 let navSolutionTitleDetails = document.querySelector(".block-page__item_header__intro__nav__solution_title__details");
 
 let menuLeft = document.querySelector(".block-page__item_header__description__menu-left");
@@ -38,6 +38,28 @@ let GetTitleDetails = () => {
     });
 }
 
+const elBurger = document.querySelector('.block-page__item_header__burger');
+const elIntro = document.querySelector('.block-page__item_header__intro');
+const burgerMenu = document.querySelector('.block-page__item_header__burger-menu');
+const burgerMenuNav = document.querySelector('.block-page__item_header__burger-menu__section');
+const burgerMenuAccess = document.querySelector('.block-page__item_header__burger-menu__access');
+const elHeaderDescription = document.querySelector('.block-page__item_header__description');
+const section = document.querySelector('.block-page__item_section');
+const footer = document.querySelector('.block-page__item_footer');
+
+const ShowBurgerMenu = () => {
+    elBurger.addEventListener('click', () => {
+        burgerMenu.style.display = 'block';
+        burgerMenuNav.style.display = 'block';
+        burgerMenuAccess.style.display = 'block';
+        burgerMenu.style.marginLeft = "0";
+        elHeaderDescription.style.marginLeft = "-1500px";
+        section.style.display = 'none';
+        footer.style.display = 'none';
+        elIntro.style.display = 'none';
+    })
+}
+
 const media1 = window.matchMedia("(min-width:1024px) and (max-width: 1200px)");
 const media2 = window.matchMedia("(min-width:768px) and (max-width: 1000px)");
 const media3 = window.matchMedia("(min-width:375px) and (max-width: 700px)");
@@ -47,14 +69,14 @@ if (media1.matches) {
     console.log("min-width:1024px")    
 } else {
     if (media2.matches) {
-        GetTitleDetails("min-width:768px");
+        ShowBurgerMenu();
         console.log("min-width:768px")
     } else {
         if (media3.matches) {
-            GetTitleDetails("min-width:375px");
+            ShowBurgerMenu();
             console.log("min-width:375px")
         } else {
-            GetTitleDetails("decktop");
+            GetTitleDetails();
         }
     }
 }
