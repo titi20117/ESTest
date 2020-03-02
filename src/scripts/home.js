882d3d12bc10b42d98a25d2af7afd8119c4456d1
Fernand Elisee Moudio Moudio <fernandmoudio1@gmail.com>
2020-03-02 06:21:24 +0300

    all change done

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -38,45 +38,50 @@ let GetTitleDetails = () => {
     });
 }
 
-const elBurger = document.querySelector('.block-page__item_header__burger');
-const elIntro = document.querySelector('.block-page__item_header__intro');
-const burgerMenu = document.querySelector('.block-page__item_header__burger-menu');
-const burgerMenuNav = document.querySelector('.block-page__item_header__burger-menu__section');
-const burgerMenuAccess = document.querySelector('.block-page__item_header__burger-menu__access');
-const elHeaderDescription = document.querySelector('.block-page__item_header__description');
-const section = document.querySelector('.block-page__item_section');
-const footer = document.querySelector('.block-page__item_footer');
-
-const ShowBurgerMenu = () => {
-    elBurger.addEventListener('click', () => {
-        burgerMenu.style.display = 'block';
-        burgerMenuNav.style.display = 'block';
-        burgerMenuAccess.style.display = 'block';
-        burgerMenu.style.marginLeft = "0";
-        elHeaderDescription.style.marginLeft = "-1500px";
-        section.style.display = 'none';
-        footer.style.display = 'none';
-        elIntro.style.display = 'none';
-    })
-}
+const elBurger = document.querySelector('.block-page__item_header__burger img');
+const page = document.querySelector('.block-page');
+const elClose = document.querySelector('.block-page__item_header__burger-menu__head__close');
+const myModal = document.getElementById('myModal');
 
 const media1 = window.matchMedia("(min-width:1024px) and (max-width: 1200px)");
 const media2 = window.matchMedia("(min-width:768px) and (max-width: 1000px)");
 const media3 = window.matchMedia("(min-width:375px) and (max-width: 700px)");
 
-if (media1.matches) {
-    GetTitleDetails();
-    console.log("min-width:1024px")    
-} else {
-    if (media2.matches) {
-        ShowBurgerMenu();
-        console.log("min-width:768px")
+let ResizeScreen = () => {
+    if (media1.matches) {
+        myModal.style.display = "none";
+        GetTitleDetails();
+        console.log("min-width:1024px")    
     } else {
-        if (media3.matches) {
-            ShowBurgerMenu();
-            console.log("min-width:375px")
+        if (media2.matches) {
+            elBurger.addEventListener('click', () => {
+                myModal.style.display = "block";
+                page.style.backgroundColor = "rgba(0,0,0,0.4)";
+                page.style.overflow = "hidden";
+                console.log('burger clicked');
+            });
+            
+            elClose.addEventListener('click', () => {
+                myModal.style.display = "none";
+                page.style.backgroundColor = "#fff";
+                page.style.overflow = "visible";
+            });
+            
+            // window.addEventListener('click', () => {
+            //     myModal.style.display = "none";
+            // });
+            console.log("min-width:768px");
         } else {
-            GetTitleDetails();
+            if (media3.matches) {
+                myModal.style.display = "none";
+                console.log("min-width:375px")
+            } else {
+                myModal.style.display = "none";
+                GetTitleDetails();
+            }
         }
     }
 }
+window.addEventListener('resize', ResizeScreen(), false);
+
+
